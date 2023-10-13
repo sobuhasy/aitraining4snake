c3c28cddc74d90ebc4e289d63312ac8fa9728550
SobuHasy <79106639+sobuhasy@users.noreply.github.com>
2023-10-13 15:12:23 +0200

    Removed the init game state in order to (hopefully) plot the graph for the training rate

--- snake_game.py
+++ snake_game.py
@@ -43,17 +43,6 @@ class SnakeGameAI:
         self.clock = pygame.time.Clock()
         self.reset()
         
-        # init game state
-        self.direction = Direction.RIGHT
-        
-        self.head = Point(self.w/2, self.h/2)
-        self.snake = [self.head, 
-                      Point(self.head.x-BLOCK_SIZE, self.head.y),
-                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
-        
-        self.score = 0
-        self.food = None
-        self._place_food()
 
     def reset(self):
         self.direction = Direction.RIGHT
