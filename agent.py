ec1892fea027021fc55bc1ecfa9cdf33af601df4
SobuHasy <79106639+sobuhasy@users.noreply.github.com>
2023-10-13 12:01:35 +0200

    Made the movements of the snake controlled by AI

--- agent.py
+++ agent.py
@@ -19,7 +19,51 @@ class Agent:
 
 
     def get_state(self, game):
-        pass
+        head = game.snake[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+
+        dir_l = game.direction = Direction.LEFT
+        dir_r = game.direction = Direction.RIGHT
+        dir_u = game.direction = Direction.UP
+        dir_d = game.direction = Direction.DOWN
+
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_d)),
+
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_d)),
+
+             # Danger left
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
+            (dir_l and game.is_collision(point_d)),
+
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+
+            # Food location
+            game.food.x < game.head.x # food left
+            game.food.x > game.head.x # food right
+            game.food.y < game.head.y # food up
+            game.food.y > game.head.y # food down
+
+        ]
+
+
 
     def remember(self, state, action, reward, next_state, done):
         pass
